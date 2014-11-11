35484495581493423f7c72b2c8332657cac5af02
kahn-aar <nicolas.martinr@gmail.com>
2014-11-11 18:21:22 -0500

    AJout des diffents cas d'appels Webservice

--- Client/src/com/client/enigmas_quest/constants/EnigmasConstants.java
+++ Client/src/com/client/enigmas_quest/constants/EnigmasConstants.java
@@ -1,5 +1,11 @@
 package com.client.enigmas_quest.constants;
 
 public class EnigmasConstants {
-	public static final int REST_GET_ALL_POSITIONS = 1;
+	public static final int REST_AUTHENTICATION = 0;
+	public static final int REST_ACOUNT_CREATION = 1;
+	public static final int REST_GET_ALL_POSITIONS = 2;
+	public static final int REST_GET_ENIGMA = 3;
+	public static final int REST_GET_PLAYER_STATS = 4;
+	public static final int REST_POST_ENIGMA_RESPONSE = 5;
+	public static final int REST_POST_POS_PLAYER = 6;
 }
--- Client/src/com/client/enigmas_quest/service/RequestRESTAsync.java
+++ Client/src/com/client/enigmas_quest/service/RequestRESTAsync.java
@@ -10,6 +10,18 @@ public class RequestRESTAsync extends AsyncTask<Integer, Void, String> {
 	protected String doInBackground(Integer... arg0) {
 		switch(arg0[0]) {
 		case EnigmasConstants.REST_GET_ALL_POSITIONS:break;
+		case EnigmasConstants.REST_AUTHENTICATION:
+			// /player/authentification?login=xxx&pasword=xxx
+			break;
+		case EnigmasConstants.REST_ACOUNT_CREATION:
+			// /player/authentification?login=xxx&pasword=xxx
+			break;
+		case EnigmasConstants.REST_GET_ENIGMA:
+			// /player/authentification?login=xxx&pasword=xxx
+			break;
+		case EnigmasConstants.REST_GET_PLAYER_STATS:
+			// /player/authentification?login=xxx&pasword=xxx
+			break;
 		}
 		return null;
 	}
